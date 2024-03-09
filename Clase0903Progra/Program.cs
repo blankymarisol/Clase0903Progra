089f3a35b94947c45f6fccd21def497044b13b97
blanksss <marrophph@gmail.com>
2024-03-09 12:36:46 -0600

    Agregar archivos de proyecto.

---
+++ Clase0903Progra/Program.cs
@@ -0,0 +1,31 @@
+﻿//static int minymax();  //ejercicio1
+//{
+//    int num, max = int.MinValue, min = int.MaxValue;
+//    do
+//    {
+//        Console.WriteLine("Ingrese su numero");
+//        num = Convert.ToInt32(Console.ReadLine());
+
+//        if (num != 0)
+//        {
+//            if (num > max)
+//            {
+//                max = num;
+//            }
+//            if (num < min)
+//            {
+//                min = num;
+//            }
+//        }
+//    }
+//    while (num != 0);
+
+//    Console.WriteLine("El numero maximo es: " + max);
+//    Console.WriteLine("El numero minimo es: " + min);
+//}
+//static int adivinar(); //ejercicio2
+//{
+//    Console.WriteLine("Piense en un numero");
+//    Console.WriteLine("Multipliquelo por ")
+
+//}
